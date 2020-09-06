47a797428a5b0f0ffce8b688fa3e1ded5616aee3
Patrick Tang <patricksptang@gmail.com>
2020-09-05 21:48:51 -0700

    Fixed bug

--- step-web/src/main/webapp/js/backbone/views/view_quick_lexicon.js
+++ step-web/src/main/webapp/js/backbone/views/view_quick_lexicon.js
@@ -202,7 +202,7 @@ var QuickLexicon = Backbone.View.extend({
     _renderBriefMorphItem: function (morphInfo, title, param) {
         if(morphInfo && param && morphInfo[param]) {
 			var morphValue = morphInfo[param];
-			var local_var_name = param.toLowerCase() + "_" + morphValue.toLowerCase().replaceAll(" ", "_");
+			var local_var_name = param.toLowerCase() + "_" + morphValue.toLowerCase().replace(/ /g, "_");
 			morphValue += (__s[local_var_name]) ? " (" + __s[local_var_name] + ") " : " ";
 			return morphValue;
         }
