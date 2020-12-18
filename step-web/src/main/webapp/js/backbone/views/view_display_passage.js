a4aecd616ed1b97617819a70acb54bfe6dd138a1
Patrick Tang <patricksptang@gmail.com>
2020-12-17 17:33:46 -0800

    test

--- step-web/src/main/webapp/js/backbone/views/view_display_passage.js
+++ step-web/src/main/webapp/js/backbone/views/view_display_passage.js
@@ -442,11 +442,12 @@ var PassageDisplayView = DisplayView.extend({
                         events: {
                             render: function (event, api) {
                                 $(api.elements.titlebar).css("padding-right", "0px");
+								$(api.elements.titlebar).css("height", "40px");
                                 $(api.elements.titlebar)
-                                    .prepend($('<span class="glyphicon glyphicon-new-window openRefInColumn"></span>')
+                                    .prepend($('<span class="glyphicon glyphicon-new-window openRefInColumn" style="font-size:20"></span>')
                                         .click(function () {
                                             step.util.createNewLinkedColumnWithScroll(self.model.get("passageId"), api.get("content.osisId"), true, null, event);
-                                        })).prepend($('<button type="button" class="close" aria-hidden="true">&times;</button>').click(function () {
+                                        })).prepend($('<button type="button" class="close" style="font-size:40px" aria-hidden="true">&times;</button>').click(function () {
                                         api.hide();
                                     }));
                             },
