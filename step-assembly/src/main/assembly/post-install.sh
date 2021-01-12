fd82ced1bebb67f55b61ff6689adaa31fc2658b9
Patrick Tang <patricksptang@gmail.com>
2021-01-12 12:01:31 -0800

    Add changes to download of Linux and sync with master branch

--- step-assembly/src/main/assembly/post-install.sh
+++ step-assembly/src/main/assembly/post-install.sh
@@ -8,5 +8,15 @@ chmod -R 777 /opt/step/homes >> /var/log/step-post-install.log
 echo "Linking files" >> /var/log/step-post-install.log
 ln -sf /opt/step/homes/sword ~/.sword >> /var/log/step-post-install.log
 ln -sf /opt/step/homes/jsword ~/.jsword >> /var/log/step-post-install.log
-ln -sf /opt/step/step.desktop ~/Desktop/step.desktop >> /var/log/step-post-install.log
+[ -d "~/Desktop" ] && ln -sf /opt/step/step.desktop ~/Desktop/step.desktop >> /var/log/step-post-install.log
 
+origUser=`pstree -lu -s $$ | grep --max-count=1 -o '([^)]*)' | head -n 1 | sed 's/(//' | sed 's/)//'`
+if [ ! -z "$origUser" ]
+then
+    [ -d "/home/$origUser/Desktop" ] && ln -sf /opt/step/step.desktop /home/$origUser/Desktop/step.desktop  >> /var/log/step-post-install.log
+    [ -d "/home/$origUser" ] && ln -sf /opt/step/homes/sword /home/$origUser/.sword >> /var/log/step-post-install.log
+    [ -d "/home/$origUser" ] && ln -sf /opt/step/homes/jsword /home/$origUser/.jsword >> /var/log/step-post-install.log
+fi
+echo ""
+echo "Click on the STEP icon on the desktop to start STEP."
+echo "If the STEP icon is not available, enter \"step\" at the command line."
