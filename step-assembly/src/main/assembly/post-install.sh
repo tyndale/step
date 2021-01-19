0cc72cc0119a5cc8f00c9fd58bc26830f6eaeb21
Patrick Tang <patricksptang@gmail.com>
2021-01-19 11:21:43 -0800

    Updates for Linux install

--- step-assembly/src/main/assembly/post-install.sh
+++ step-assembly/src/main/assembly/post-install.sh
@@ -5,10 +5,15 @@ if [ "$user" = "root" ]; then
 	echo "Changing permissions to allow read/write to this directory"
 	chmod -R 777 /opt/step/homes >> /var/log/step-post-install.log
 	chmod +x /opt/step/post-install.sh
+	rm -f /opt/step/step-install4j
+	mv /opt/step/step /opt/step/step-install4j
+	cp /opt/step/runStep.sh step
+	chmod +x /opt/step/step
+	chmod +x /opt/step/step-install4j
 	echo "Linking files" >> /var/log/step-post-install.log
 	echo ""
 	echo "Click on the STEP icon on the desktop to start STEP."
-	echo "If there is no STEP icon, enter \"/opt/step/step &\" at the command line."
+	echo "If there is no STEP icon, enter \"step\" or \"/opt/step/step\" at the command line."
 	user=`pstree -lu -s $$ | grep --max-count=1 -o '([^)]*)' | head -n 1 | sed 's/(//' | sed 's/)//'`
 fi
 if [ ! -z "$user" ] && [ "$user" != "root" ]; then
