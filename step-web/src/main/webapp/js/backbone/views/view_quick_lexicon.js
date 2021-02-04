818b31a72092605700fcda0097c245a7d71c780c
Patrick Tang <patricksptang@gmail.com>
2021-02-03 20:01:08 -0800

    new UI move the quick lexicon on the top to a little lower

--- step-web/src/main/webapp/js/backbone/views/view_quick_lexicon.js
+++ step-web/src/main/webapp/js/backbone/views/view_quick_lexicon.js
@@ -111,7 +111,7 @@ var QuickLexicon = Backbone.View.extend({
 					fontClass: step.util.ui.getFontForStrong(self.strong),
 					view: self }));
                 if (self.position > 0.66) {
-                    lexicon.css({"top": "0", "bottom": "auto"});
+                    lexicon.css({"top": "32", "bottom": "auto"});
                 }
                 self.displayQuickDef(lexicon);
             }
