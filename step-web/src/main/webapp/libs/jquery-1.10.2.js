e647e25a6895542cde696ed6b03e6ab84af2719e
Patrick Tang <patricksptang@gmail.com>
2021-05-19 23:53:32 -0700

    test jquery change to remove warnings

--- step-web/src/main/webapp/libs/jquery-1.10.2.js
+++ step-web/src/main/webapp/libs/jquery-1.10.2.js
@@ -9787,3 +9787,22 @@
     }
 
 })( window );
+
+jQuery.event.special.touchstart = {
+  setup: function( _, ns, handle ){
+    if ( ns.includes("noPreventDefault") ) {
+      this.addEventListener("touchstart", handle, { passive: false });
+    } else {
+      this.addEventListener("touchstart", handle, { passive: true });
+    }
+  }
+};
+jQuery.event.special.touchmove = {
+  setup: function( _, ns, handle ){
+    if ( ns.includes("noPreventDefault") ) {
+      this.addEventListener("touchmove", handle, { passive: false });
+    } else {
+      this.addEventListener("touchmove", handle, { passive: true });
+    }
+  }
+};
